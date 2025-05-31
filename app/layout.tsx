4ac2e5bd6bd030950352a7d47c8432cc03d32ad1
Yash Bhalchandra <yashb1004@gmail.com>
2025-05-31 17:25:26 -0400

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/ThemeProvider';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ 
   subsets: ['latin'],
